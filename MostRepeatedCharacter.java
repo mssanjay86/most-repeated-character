65b57955f6ff26c4ac2c01fbd6d2e4aa67b59912
Sanjay <mssanjay666@gmail.com>
2023-01-26 20:19:01 +0530

    First commit

---
+++ MostRepeatedCharacter.java
@@ -0,0 +1,23 @@
+public class App {
+
+    public static void main(String[] args) throws Exception {
+        String str = "santhosh";
+        char[] charArr = str.toCharArray();
+        int count = 1;
+        char ch = charArr[0];
+        int max = 0;
+        for (int i = 0; i < charArr.length; i++) {
+            for (int j = i + 1; j < charArr.length; j++) {
+                if (charArr[i] == charArr[j] && charArr[j] != '$') {
+                    count++;
+                    charArr[j] = '$';
+                    if (count > max) {
+                        max = count;
+                        ch = charArr[i];
+                    }
+                }
+            }
+        }
+        System.out.println("Maximum repeated character is :" + ch);
+    }
+}
